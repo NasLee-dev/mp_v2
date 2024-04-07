4230e9b7b3e062bc39d43e42fb0768c0ec68b230
wootaiklee <fined000@naver.com>
2024-04-07 22:58:25 +0900

    npm

--- src/components/auth/AuthGuard.tsx
+++ src/components/auth/AuthGuard.tsx
@@ -1,4 +1,4 @@
-import { useSession } from 'next-auth/react'
+import { useSession } from "next-auth/react"
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
   const { data, status } = useSession()
