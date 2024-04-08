3887474cccb28a2acef21dc3f74b9778358c3b03
wootaiklee <fined000@naver.com>
2024-04-08 21:54:48 +0900

    ts

--- src/pages/auth/signIn.tsx
+++ src/pages/auth/signIn.tsx
@@ -25,65 +25,66 @@ function SignInPage({
             로그인을 진행해주세요!
           </Text>
           <Spacing direction="vertical" size={80} />
-          {Object.values(providers).map((provider) => (
-            <>
-              <div
-                key={provider.id}
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  width: '100%',
-                  height: '50px',
-                  borderRadius: '8px',
-                  justifyContent: 'center',
-                  color: provider.id === 'google' ? 'white' : 'black',
-                  backgroundColor:
-                    provider.id === 'google'
-                      ? '#4285F4'
-                      : provider.id === 'kakao'
-                        ? '#FFEB00'
-                        : '#3C1E1E',
-                  cursor: 'pointer',
-                }}
-                onClick={() =>
-                  signIn(provider.id, {
-                    callbackUrl: '/',
-                  })
-                }
-              >
+          {providers &&
+            Object.values(providers).map((provider) => (
+              <>
                 <div
+                  key={provider.id}
                   style={{
                     display: 'flex',
+                    flexDirection: 'column',
                     alignItems: 'center',
-                    gap: '10px',
-                  }}
-                >
-                  <Image
-                    src={
+                    width: '100%',
+                    height: '50px',
+                    borderRadius: '8px',
+                    justifyContent: 'center',
+                    color: provider.id === 'google' ? 'white' : 'black',
+                    backgroundColor:
                       provider.id === 'google'
-                        ? '/images/GoogleLogo.png'
+                        ? '#4285F4'
                         : provider.id === 'kakao'
-                          ? '/images/KakaoLogo.png'
-                          : '/naver.png'
-                    }
-                    width={30}
-                    height={30}
-                    alt={provider.name}
-                  />
-                  <Text
+                          ? '#FFEB00'
+                          : '#3C1E1E',
+                    cursor: 'pointer',
+                  }}
+                  onClick={() =>
+                    signIn(provider.id, {
+                      callbackUrl: '/',
+                    })
+                  }
+                >
+                  <div
                     style={{
-                      color: provider.id === 'google' ? 'white' : 'black',
+                      display: 'flex',
+                      alignItems: 'center',
+                      gap: '10px',
                     }}
-                    typography="t5"
                   >
-                    {provider.id === 'google' ? 'Google' : 'Kakao'}로 로그인
-                  </Text>
+                    <Image
+                      src={
+                        provider.id === 'google'
+                          ? '/images/GoogleLogo.png'
+                          : provider.id === 'kakao'
+                            ? '/images/KakaoLogo.png'
+                            : '/naver.png'
+                      }
+                      width={30}
+                      height={30}
+                      alt={provider.name}
+                    />
+                    <Text
+                      style={{
+                        color: provider.id === 'google' ? 'white' : 'black',
+                      }}
+                      typography="t5"
+                    >
+                      {provider.id === 'google' ? 'Google' : 'Kakao'}로 로그인
+                    </Text>
+                  </div>
                 </div>
-              </div>
-              <Spacing direction="vertical" size={10} />
-            </>
-          ))}
+                <Spacing direction="vertical" size={10} />
+              </>
+            ))}
         </Flex>
       </Flex>
     </div>
