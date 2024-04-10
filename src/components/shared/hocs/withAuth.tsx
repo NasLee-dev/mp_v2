13c4cfc2b0005ae4cebd4e7732934114efd7eef2
wootaiklee <fined000@naver.com>
2024-04-10 22:05:48 +0900

    hocs update

--- src/components/shared/hocs/withAuth.tsx
+++ src/components/shared/hocs/withAuth.tsx
@@ -1,15 +1,18 @@
+import { userAtom } from '@/store/atom/user'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import { ComponentType } from 'react'
+import { useRecoilState, useRecoilValue } from 'recoil'
 
 function withAuth<Props = Record<string, never>>(
   WrappedComponent: ComponentType<Props>,
 ) {
   return function AuthenticatedComponent(props: Props) {
     const router = useRouter()
+    const user = useRecoilValue(userAtom)
     const { data, status } = useSession()
-    if (status !== 'loading' && data == null) {
-      router.replace('/signIn')
+    if ((status !== 'loading' && data == null) || user?.email === '') {
+      router.replace('/auth/signin')
       return null
     }
     return <WrappedComponent {...(props as any)} />
